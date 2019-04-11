a6d2f0ca2117499bcc0d83c746172c0c944abcdd
yaoweifeng01 <steponeinfo@126.com>
2019-04-11 16:34:15 +0800

    add test py

--- cyber/docs/cyber_py.rst
+++ cyber/docs/cyber_py.rst
@@ -1,6 +1,8 @@
 API
 ===
 
+.. automodule:: run
+
 .. automodule:: cyber
    :members:
 
---
+++ cyber/run.py
@@ -0,0 +1,25 @@
+def run11(name):
+    """
+    this fun1 is how we run
+
+    :param name name of people who runs
+    """
+    print(name)
+    print name, " python27"
+
+def run2(name):
+    """
+    this fun2 is how we run
+
+    :param name name of people who runs
+    """
+    print(name)
+    print("hahahah")
+
+def run3(name):
+    """
+    this fun3 is how we run
+
+    :param name name of people who runs
+    """
+    print(name)
